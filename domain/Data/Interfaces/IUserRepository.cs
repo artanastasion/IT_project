bafc20f4022c6cf928362a5dc7162f0891b69e57
artanastasion <103057420+artanastasion@users.noreply.github.com>
2022-12-21 02:32:18 +1000

    task2 v.1

---
+++ domain/Data/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using domain.Data.Models;
+
+namespace domain.Data.Interfaces;
+
+public interface IUserRepository
+{
+    IEnumerable<Users> GetUserList();
+    Users GetByLogin(string login);
+    Users GetUser(int id);
+    void Create(Users item);
+    void Update(Users item);
+    void Delete(int id);    
+    void Save();
+}
---
+++ domain/Data/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using domain.Data.Interfaces;
+using domain.Data.Models;
+
+namespace domain.Data.Services;
+
+public class UserService
+{
+    private readonly IUserRepository _repository;
+
+    public UserService(IUserRepository repository)
+    {
+        _repository = repository;
+    }
+        
+    public Result<Users> GetUserByLogin(string login)
+    {
+        if (string.IsNullOrEmpty(login))
+            return Result.Fail<Users>("Логин не был указан");
+
+        var user = _repository.GetByLogin(login);
+
+        return user is null ? Result.Fail<Users>("Пользователь не найден") : Result.Ok(user);
+    }
+}
