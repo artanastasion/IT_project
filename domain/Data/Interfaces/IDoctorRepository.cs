d8ec9614542e213a4f012ad46a2762f7d939fd32
artanastasion <103057420+artanastasion@users.noreply.github.com>
2022-12-22 20:37:31 +1000

    add logic for doctor

---
+++ domain/Data/Interfaces/IDoctorRepository.cs
@@ -0,0 +1,17 @@
+﻿using domain.Data.Models;
+
+namespace domain.Data.Interfaces;
+
+public interface IDoctorRepository
+{
+    IEnumerable<Doctor> GetDoctorList();
+    Doctor GetNameDoctor(string name);
+
+    Doctor GetSpecialisationDoctor(Specialization item);
+    Doctor GetDoctorId(int id);
+    Doctor Create(Doctor item);
+    Doctor Delete(int id);    
+    Doctor Save();
+    Doctor Update(Doctor item);
+
+}
