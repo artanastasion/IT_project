e23fa24ca55f6114a318cc620eda9a36746fb25a
artanastasion <103057420+artanastasion@users.noreply.github.com>
2023-01-24 04:55:35 +1000

    add controllers, add views, setting program.cs

--- IT_project/Program.cs
+++ IT_project/Program.cs
@@ -1,6 +1,61 @@
+using domain.Data.Models;
+using domain.Data.Interfaces;
+using domain.Data.Services;
+using domain;
+using dbase;
+using dbase.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DATABASE_URL")));
+builder.Services.AddDbContext<ApplicationContext>(options =>
+    options.EnableSensitiveDataLogging(true));
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IScheduleRepository, ScheduleRepository>();
+builder.Services.AddTransient<IReceptionRepository, ReceptionRepository>();
+builder.Services.AddTransient<IDoctorRepository, DoctorRepository>();
+builder.Services.AddTransient<ISpecializationRepository, SpecializationRepository>();
+builder.Services.AddTransient<UserInteractor>();
+builder.Services.AddTransient<DoctorInteractor>();
+builder.Services.AddTransient<ScheduleInteractor>();
+builder.Services.AddTransient<ReceptionInteractor>();
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.MapControllers();
+
 
 app.Run();
